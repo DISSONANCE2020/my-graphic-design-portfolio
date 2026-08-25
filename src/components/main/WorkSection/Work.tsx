560df5b28775587477322804ec11e9497e5988b4
Jose Gabriel Cruz <132447240+DISSONANCE2020@users.noreply.github.com>
2026-08-25 19:16:36 +0800

    Added Responsive rightContainer Grid and Carousel to Work Section

--- src/components/main/WorkSection/Work.tsx
+++ src/components/main/WorkSection/Work.tsx
@@ -1,10 +1,40 @@
-import { Text, Accordion } from "@mantine/core";
-import { accordionHeader, accordionData } from "./workContent";
+import { useState, useEffect, useMemo } from "react";
+import { Text, Accordion, Grid } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import {
+  accordionHeader,
+  accordionData,
+  gridDataDefault,
+  gridDataMobile,
+} from "./workContent";
 import styles from "./Work.module.css";
 
 import ContactButtons from "../../reusable/ContactButtons/ContactButtons";
 
 export default function Work() {
+  const [draggableStatus, setDraggableStatus] = useState(false);
+  const [gridSpan, setGridSpan] = useState(6);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 992) {
+        setDraggableStatus(false);
+        setGridSpan(6);
+      } else if (window.innerWidth <= 992) {
+        setDraggableStatus(true);
+        setGridSpan(4);
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const items = accordionData.map((item) => (
     <Accordion.Item key={item.label} value={item.description}>
       <Accordion.Control className={styles.accordionLabel}>
@@ -15,6 +45,62 @@ export default function Work() {
       </Accordion.Panel>
     </Accordion.Item>
   ));
+
+  const gridContent = useMemo(() => {
+    switch (draggableStatus) {
+      case true:
+        return (
+          <Carousel
+            className={styles.carouselContainer}
+            classNames={{ controls: styles.carouselControls }}
+            emblaOptions={{
+              watchDrag: draggableStatus,
+            }}
+          >
+            {gridDataMobile.map((slideItems, slideIndex) => (
+              <Carousel.Slide key={slideIndex}>
+                <Grid grow className={styles.gridContainer}>
+                  {slideItems.map((item) => (
+                    <Grid.Col
+                      key={item.id}
+                      span={gridSpan}
+                      className={styles.imageContainer}
+                    >
+                      <img src={item.url} alt={item.alt} />
+                    </Grid.Col>
+                  ))}
+                </Grid>
+              </Carousel.Slide>
+            ))}
+          </Carousel>
+        );
+
+      case false:
+        return (
+          <Carousel
+            className={styles.carouselContainer}
+            classNames={{ controls: styles.carouselControls }}
+            emblaOptions={{
+              watchDrag: draggableStatus,
+            }}
+          >
+            <Carousel.Slide>
+              <Grid grow className={styles.gridContainer}>
+                {gridDataDefault.map((item) => (
+                  <Grid.Col
+                    span={gridSpan}
+                    className={styles.imageContainer}
+                    key={item.id}
+                  >
+                    <img src={item.url} alt={item.alt} />
+                  </Grid.Col>
+                ))}
+              </Grid>
+            </Carousel.Slide>
+          </Carousel>
+        );
+    }
+  }, [draggableStatus]);
 
   return (
     <section className={styles.workContainer} id="work">
@@ -27,7 +113,7 @@ export default function Work() {
           <ContactButtons />
         </div>
       </div>
-      <div className={styles.rightContainer}>Right Container</div>
+      <div className={styles.rightContainer}>{gridContent}</div>
     </section>
   );
 }
--- src/components/main/WorkSection/workContent.tsx
+++ src/components/main/WorkSection/workContent.tsx
@@ -15,3 +15,72 @@ export const accordionData = [
     description: "Yes. Please.",
   },
 ];
+
+export const gridDataMobile = [
+  [
+    {
+      id: "1",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 1",
+    },
+    {
+      id: "2",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 2",
+    },
+    {
+      id: "3",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 3",
+    },
+    {
+      id: "4",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 4",
+    },
+    {
+      id: "5",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 5",
+    },
+    {
+      id: "6",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 6",
+    },
+  ],
+  [
+    {
+      id: "7",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 7",
+    },
+    {
+      id: "8",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 8",
+    },
+    {
+      id: "9",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 9",
+    },
+    {
+      id: "10",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 10",
+    },
+    {
+      id: "11",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 11",
+    },
+    {
+      id: "12",
+      url: "https://via.placeholder.com/300x200",
+      alt: "Placeholder Image 12",
+    },
+  ],
+];
+
+export const gridDataDefault = gridDataMobile.flat();
